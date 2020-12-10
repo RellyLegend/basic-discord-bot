c46e09ef588aa2f5accafc889d5a1d2708f449d0
RellyLegend <75770996+RellyLegend@users.noreply.github.com>
2020-12-10 05:48:11 +0000

    Update index.js
    Adding Information command.

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const pingms = message.channel.send(":ping_pong: Pinging...")
 pingms.delete();
 message.channel.send(`🏓Latency is ${pingms.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
 
-}
+} else if(message.content === `${prefix}info`)
+  const infoEmbed = new MessageEmbed();
+  .setColor("RANDOM")
+  .setDescription(`Hey there! My name is ${process.env.BOT_NAME}! I am a Discord bot that is [easy-to-hosthost](https://github.com/RellyLegend/basic-discord-bot) yourself, I am owned by ${process.env.BOT_OWNER} and created in the Relly Developments! My prefix is ${process.env.BOT_PREFIX}! View my commands by typing ${process.env.BOT_PREFIX}help`)
 });
 
 client.login(process.env.BOT_TOKEN)
